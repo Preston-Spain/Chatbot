bba35e338fb2a0f4144c66821263cd62bebf1162
Preston-Spain <spainpreston22@gmail.com>
2025-01-13 12:59:54 -0600

    Update Network.java

--- Network.java
+++ Network.java
@@ -3,7 +3,16 @@ package Chatbot;
 // import
 
 public class Network{
-    public static double[] LSTM(double Xt, double lCt, double lht) {
+
+    public static double GetIDVar(int ID, String class) {
+        // Place holder
+    }
+
+    public static double PushIDVar(int ID, String class, double x) {
+        // Place holder
+    }
+
+    public static double[] LSTM(int ID, double Xt, double lCt, double lHt) {
         double sigmoid(double x) {
             return 1 / (1 + Math.exp(x));
         }
@@ -12,6 +21,13 @@ public class Network{
             return Math.tanh(x);
         }
 
+        double f = sigmoid((GetIDVar(ID, "fWx") * Xt) + (GetIDVar(ID, "fWlh") * lHt) + GetIDVar(ID, "fB"));
+        double i = sigmoid((GetIDVar(ID, "iWx") * Xt) + (GetIDVar(ID, "iWlh") * lHt) + GetIDVar(ID, "iB"));
+        double o = sigmoid((GetIDVar(ID, "oWx") * Xt) + (GetIDVar(ID, "oWlh") * lHt) + GetIDVar(ID, "oB"));
+
+        double c = tanh  ((GetIDVar(ID, "cWx") * Xt) + (GetIDVar(ID, "cWlh") * lHt) + GetIDVar(ID, "cB"));
+
+        double C = (f * lCt) + (i * pc);
         return [1,1,1];
     }
 }
