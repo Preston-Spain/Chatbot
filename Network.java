7cfa6a809b7977ee7ae8bd8d7ca5a23d75540255
Preston-Spain <spainpreston22@gmail.com>
2025-01-13 09:51:56 -0600

    Add files via upload

---
+++ Network.java
@@ -0,0 +1,17 @@
+package Chatbot;
+
+// import
+
+public class Network{
+    public static double[] LSTM(double Xt, double lCt, double lht) {
+        double sigmoid(double x) {
+            return 1 / (1 + Math.exp(x));
+        }
+
+        double tanh(double x) {
+            return Math.tanh(x);
+        }
+
+        return [1,1,1];
+    }
+}
